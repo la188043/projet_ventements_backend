a82dbae721f60b985a4ac491409be25391c3d3c8
Lorenzo <la188504@student.helha.be>
2020-11-19 15:49:44 +0100

    Changement nom database

--- Infrastructure/SqlServer/Shared/Database.cs
+++ Infrastructure/SqlServer/Shared/Database.cs
@@ -5,7 +5,7 @@ namespace Infrastructure.SqlServer.Shared
     public class Database
     {
         private readonly static string ConnectionString =
-            @"Server=LAPTOP-KN5RCJJ2\SQLEXPRESS;Database=sql_ventements_project;Integrated Security=SSPI";
+            @"Server=LORENZUS\SQLEXPRESS;Database=sql_ventements_project;Integrated Security=SSPI";
 
         public static SqlConnection GetConnection()
         {

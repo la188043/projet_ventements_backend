b67295f7c247a9397318a7d110ecc5f77914ef53
Sébastien Counen <sebastien@LAPTOP-KN5RCJJ2.localdomain>
2020-11-19 15:27:30 +0100

    Ajout sqlclient + fichier Database.cs

---
+++ Infrastructure/SqlServer/Shared/Database.cs
@@ -0,0 +1,15 @@
+﻿using System.Data.SqlClient;
+
+namespace Infrastructure.SqlServer.Shared
+{
+    public class Database
+    {
+        private readonly static string ConnectionString =
+            @"Server=LAPTOP-KN5RCJJ2\SQLEXPRESS;Database=sql_ventements_project;Integrated Security=SSPI";
+
+        public static SqlConnection GetConnection()
+        {
+            return new SqlConnection(ConnectionString);
+        }
+    }
+}

45daa1f41181e26abdae10fd2efc3cf43841dd5b
la188043 <la188043@student.helha.be>
2020-11-20 11:14:12 +0100

    Infrastructure layer: Wrote sql server queries for user

--- Infrastructure/SqlServer/Users/UserSqlServer.cs
+++ Infrastructure/SqlServer/Users/UserSqlServer.cs
@@ -11,5 +11,19 @@
         public static readonly string ColPassword = "encryptedPassword";
         public static readonly string ColGender = "gender";
         public static readonly string ColAdmin = "administrator";
+
+        public static readonly string ReqQuery = $"SELECT * FROM {TableName}";
+
+        public static readonly string ReqGetById = ReqQuery + $" WHERE {ColId} = @{ColId}";
+
+        public static readonly string ReqCreate = $@"
+            INSERT INTO {TableName} 
+            ({ColFirstname}, {ColLastname}, {ColBirthDate}, {ColEmail}, {ColPassword}, {ColGender})
+            OUTPUT INSERTED.{ColId}
+            VALUES
+            (@{ColFirstname}, @{ColLastname}, @{ColBirthDate}, @{ColEmail}, @{ColPassword}, @{ColGender})
+        ";
+
+        public static readonly string ReqAuthenticate = ReqQuery + $" WHERE {ColEmail} = @{ColEmail}";
     }
 }

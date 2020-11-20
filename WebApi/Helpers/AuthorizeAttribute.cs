72fa4f0d532d4a019f937f8aa2e0ef4bb524c156
la188043 <la188043@student.helha.be>
2020-11-20 11:47:28 +0100

    WebApi layer: helpers (AuthorizeAttribute, JwtMiddleware, PasswordEncryption)

---
+++ WebApi/Helpers/AuthorizeAttribute.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Application.Services.Users.Dto;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace WebApi.Helpers
+{
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method)]
+    public class AuthorizeAttribute : Attribute, IAuthorizationFilter
+    {
+        public void OnAuthorization(AuthorizationFilterContext context)
+        {
+            var user = (OutputDtoQueryUser) context.HttpContext.Items["User"];
+            if (user == null)
+            {
+                context.Result = new JsonResult(new {message = "Not authorized access"})
+                {
+                    StatusCode = StatusCodes.Status401Unauthorized
+                };
+            }
+        }
+    }
+}

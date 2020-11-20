922af9b529282671ccb91c9aa6cef4220609c274
la188043 <la188043@student.helha.be>
2020-11-20 10:14:41 +0100

    Application layer: base repository interface for user

---
+++ Application/Repositories/IUserRepository.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using Domain.Users;
+
+namespace Application.Repositories
+{
+    public interface IUserRepository
+    {
+        IEnumerable<IUser> Query();
+        IUser GetById(int id);
+        IUser Create(IUser user);
+        IUser Authenticate(IUser user);
+    }
+}

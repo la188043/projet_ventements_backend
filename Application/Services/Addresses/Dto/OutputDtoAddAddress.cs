7177f941cb943bbbe898d2bd615175d025166fce
la188043 <la188043@student.helha.be>
2020-11-23 15:09:25 +0100

    Application layer: Dto add (in, out) for address

---
+++ Application/Services/Addresses/Dto/OutputDtoAddAddress.cs
@@ -0,0 +1,11 @@
+﻿namespace Application.Services.Addresses.Dto
+{
+    public class OutputDtoAddAddress
+    {
+        public int Id { get; set; }
+        public string Street { get; set; }
+        public int HomeNumber { get; set; }
+        public string Zip { get; set; }
+        public string City { get; set; }
+    }
+}

e57c886aa9a5bd749d949e9632ecbfc223b55704
la188043 <la188043@student.helha.be>
2020-11-23 15:30:23 +0100

    Infrastructure layer: address queries (sqlserver)

---
+++ Infrastructure/SqlServer/Addresses/AddressRepository.cs
@@ -0,0 +1,18 @@
+﻿using Application.Repositories;
+using Domain.Addresses;
+
+namespace Infrastructure.SqlServer.Addresses
+{
+    public class AddressRepository : IAddressRepository
+    {
+        public IAddress GetById(int id)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public IAddress Create(IAddress address)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ Infrastructure/SqlServer/Addresses/AddressSqlServer.cs
@@ -0,0 +1,20 @@
+﻿namespace Infrastructure.SqlServer.Addresses
+{
+    public class AddressSqlServer
+    {
+        public static readonly string TableName = "addressuserv";
+        public static readonly string ColId = "id";
+        public static readonly string ColStreet = "street";
+        public static readonly string ColHomeNumber = "homeNumber";
+        public static readonly string ColZip = "zip";
+        public static readonly string ColCity = "city";
+
+        public static readonly string ReqGetById = $"SELECT * FROM {TableName} WHERE {ColId} = @{ColId}";
+
+        public static readonly string ReqCreate = $@"
+            INSERT INTO {TableName} ({ColStreet}, {ColHomeNumber}, {ColZip}, {ColCity})
+            OUTPUT INSERTED.{ColId}
+            VALUES (@{ColStreet}, @{ColHomeNumber}, @{ColZip}, @{ColCity})
+        ";
+    }
+}

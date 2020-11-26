f6ec53a9da94291a2cfa8e78778d069536bb6f84
youssraaa <youssradahouane1@gmail.com>
2020-11-26 21:02:29 +0100

    Item Service Interface

---
+++ Application/Services/Items/IItemService.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using Application.Services.Items.Dto;
+
+namespace Application.Services.Items
+{
+    public interface IItemService
+    {
+        IEnumerable<OutputDtoQueryItem> Query();
+        OutputDtoQueryItem GetById(int id);
+        OutputDtoQueryItem Create(int subcategoryId,InputDtoAddItem inputDtoAddItem);
+        bool Update(int id, InputDtoUpdateItem inputDtoUpdateItem);
+        IEnumerable<OutputDtoQueryItem> GetBySubCategoryId(int id);
+        
+
+        
+    }
+}

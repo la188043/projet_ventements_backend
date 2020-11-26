bfe5b90657b021ba63aaff97876702ea4f587c24
youssraaa <youssradahouane1@gmail.com>
2020-11-26 20:40:40 +0100

    repository interface for item

---
+++ Application/Repositories/IItemRepository.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using Domain.Items;
+
+
+namespace Application.Repositories
+{
+    public interface IItemRepository
+    {
+        IEnumerable<IItem> Query();
+        IItem GetById(int id);
+        IItem Create(int subcategoryId,IItem item);
+        bool Update(int id, IItem item);
+        IEnumerable<IItem> GetBySubCategoryId(int id);
+        
+    }
+}

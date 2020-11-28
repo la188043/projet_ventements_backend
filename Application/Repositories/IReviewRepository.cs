1429ccad0ad114dca37d75fcc37741cecfa23c87
la188043 <la188043@student.helha.be>
2020-11-28 22:03:59 +0100

    Reviews: added and implemented query and getByItemId in ReviewRepository

--- Application/Repositories/IReviewRepository.cs
+++ Application/Repositories/IReviewRepository.cs
@@ -1,4 +1,5 @@
-﻿using Domain.Items;
+﻿using System.Collections.Generic;
+using Domain.Items;
 using Domain.reviews;
 using Domain.Users;
 
@@ -6,6 +7,8 @@ namespace Application.Repositories
 {
     public interface IReviewRepository
     {
+        IEnumerable<IReview> Query();
+        IEnumerable<IReview> GetByItemId(int itemId);
         IReview Create(IUser uservId, IItem itemId, IReview review);
         bool Delete(int id);
         bool Update(int id, IReview review);
--- Infrastructure/SqlServer/Reviews/ReviewRepository.cs
+++ Infrastructure/SqlServer/Reviews/ReviewRepository.cs
@@ -1,13 +1,55 @@
-﻿using Application.Repositories;
+﻿using System.Collections.Generic;
+using System.Data;
+using Application.Repositories;
 using Domain.Items;
 using Domain.reviews;
 using Domain.Users;
+using Infrastructure.SqlServer.Factories;
 using Infrastructure.SqlServer.Shared;
 
 namespace Infrastructure.SqlServer.Reviews
 {
     public class ReviewRepository : IReviewRepository
     {
+        private readonly IInstanceFromReader<IReview> _factory = new ReviewFactory();
+        public IEnumerable<IReview> Query()
+        {
+            IList<IReview> reviews = new List<IReview>();
+            using (var connection = Database.GetConnection())
+            {
+                connection.Open();
+                var cmd = connection.CreateCommand();
+                cmd.CommandText = ReviewSqlServer.ReqQuery;
+
+                var reader = cmd.ExecuteReader(CommandBehavior.CloseConnection);
+                
+                while (reader.Read())
+                    reviews.Add(_factory.CreateFromReader(reader));
+            }
+
+            return reviews;
+        }
+
+        public IEnumerable<IReview> GetByItemId(int itemId)
+        {
+            IList<IReview> reviews = new List<IReview>();
+            using (var connection = Database.GetConnection())
+            {
+                connection.Open();
+                var cmd = connection.CreateCommand();
+                cmd.CommandText = ReviewSqlServer.ReqGetByItemId;
+
+                cmd.Parameters.AddWithValue($"@{ReviewSqlServer.ColItemId}", itemId);
+
+                var reader = cmd.ExecuteReader(CommandBehavior.CloseConnection);
+                
+                while (reader.Read())
+                    reviews.Add(_factory.CreateFromReader(reader));
+            }
+
+            return reviews;
+        }
+
         public IReview Create(IUser userv, IItem item, IReview review)
         {
             var Review = new Review

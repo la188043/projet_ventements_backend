4decbe94b88b0736bc5b35f4cbf715ceadd5e5c0
vithusasat <74712497+vithusasat@users.noreply.github.com>
2020-11-28 13:15:12 +0100

    Infrastructure: Reviews : repository + sql

---
+++ Infrastructure/SqlServer/Reviews/ReviewRepository.cs
@@ -0,0 +1,68 @@
+﻿using Application.Repositories;
+using Domain.Items;
+using Domain.reviews;
+using Domain.Users;
+using Infrastructure.SqlServer.Shared;
+
+namespace Infrastructure.SqlServer.Reviews
+{
+    public class ReviewRepository:IReviewRepository
+    {
+        public IReview Create(IUser userv, IItem item, IReview review)
+        {
+            var Review = new Review
+            {
+                User = userv,
+                Item = item
+            };
+            using (var connection = Database.GetConnection())
+            {
+                connection.Open();
+                var cmd = connection.CreateCommand();
+                cmd.CommandText = ReviewSqlServer.ReqCreate;
+                
+                cmd.Parameters.AddWithValue($"@{ReviewSqlServer.ColStars}", review.Stars);
+                cmd.Parameters.AddWithValue($"@{ReviewSqlServer.ColLikes}", review.Likes);
+                cmd.Parameters.AddWithValue($"@{ReviewSqlServer.ColTitle}", review.Title);
+                cmd.Parameters.AddWithValue($"@{ReviewSqlServer.ColDescriptionReview}", review.DescriptionReview);
+                cmd.Parameters.AddWithValue($"@{ReviewSqlServer.ColItemId}", item.Id);
+                cmd.Parameters.AddWithValue($"@{ReviewSqlServer.ColUserId}", userv.Id);
+
+                Review.Id = (int) cmd.ExecuteScalar();
+            }
+
+            return Review;
+        }
+        public bool Delete(int id)
+        {
+            using (var connection = Database.GetConnection())
+            {
+                connection.Open();
+                var cmd = connection.CreateCommand();
+
+                cmd.CommandText = ReviewSqlServer.ReqDelete;
+                
+                cmd.Parameters.AddWithValue($"@{ReviewSqlServer.ColId}", id);
+                
+                return cmd.ExecuteNonQuery() > 0;
+            }
+        }
+
+        public bool Update(int id, IReview review)
+        {
+            using (var connection = Database.GetConnection())
+            {
+                connection.Open();
+                var cmd = connection.CreateCommand();
+
+                cmd.CommandText = ReviewSqlServer.ReqPut;
+
+                cmd.Parameters.AddWithValue($"@{ReviewSqlServer.ColId}", id);
+                cmd.Parameters.AddWithValue($"@{ReviewSqlServer.ColStars}", review.Stars);
+                cmd.Parameters.AddWithValue($"@{ReviewSqlServer.ColDescriptionReview}", review.DescriptionReview);
+
+                return cmd.ExecuteNonQuery() > 0;
+            }
+        }
+    }
+}
---
+++ Infrastructure/SqlServer/Reviews/ReviewSqlServer.cs
@@ -0,0 +1,26 @@
+﻿namespace Infrastructure.SqlServer.Reviews
+{
+    public class ReviewSqlServer
+    {
+        public static readonly string TableName = "review";
+        public static readonly string ColId = "id";
+        public static readonly string ColStars = "stars";
+        public static readonly string ColLikes = "likes";
+        public static readonly string ColTitle = "title";
+        public static readonly string ColDescriptionReview = "descriptionReview";
+        public static readonly string ColItemId = "itemId";
+        public static readonly string ColUserId = "uservId";
+        
+        public static readonly string ReqCreate = $@"INSERT INTO {TableName} ({ColStars}, {ColLikes}, {ColTitle}, {ColDescriptionReview}, {ColItemId}, {ColUserId}) OUTPUT 
+        Inserted.{ColId} VALUES (@{ColStars}, @{ColLikes}, @{ColTitle}, @{ColDescriptionReview}, @{ColItemId}, @{ColUserId})";
+        
+        public static readonly string ReqDelete = $@"DELETE FROM {TableName} WHERE {ColId} = @{ColId}";
+        
+        public static readonly string ReqPut = $@"
+            UPDATE {TableName} SET
+            {ColStars} = @{ColStars},
+            {ColDescriptionReview} = @{ColDescriptionReview}
+            WHERE {ColId} = @{ColId}
+        ";
+    }
+}

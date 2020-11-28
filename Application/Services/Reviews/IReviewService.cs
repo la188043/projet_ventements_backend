dd87e974bf66168c3db5691a9a183f80300f1c53
vithusasat <74712497+vithusasat@users.noreply.github.com>
2020-11-28 13:10:11 +0100

    Services: Reviews : create,delete,update

---
+++ Application/Services/Reviews/IReviewService.cs
@@ -0,0 +1,12 @@
+﻿using Application.Services.Reviews.Dto;
+
+
+namespace Application.Services.Reviews
+{
+    public interface IReviewService
+    {
+        OutputDtoAddReview Create(int uservId,int itemId,InputDtoAddReview review);
+        bool Delete(int id);
+        bool Update(int id, InputDtoUpdateReview inputDtoUpdateReview);
+    }
+}

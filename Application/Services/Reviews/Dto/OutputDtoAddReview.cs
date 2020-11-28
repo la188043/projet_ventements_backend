2091fc9eaa18bcef58b9448f7918b48657ee52ba
la188043 <la188043@student.helha.be>
2020-11-28 21:06:58 +0100

    Reviews: updated output review dto

--- Application/Services/Reviews/Dto/OutputDtoAddReview.cs
+++ Application/Services/Reviews/Dto/OutputDtoAddReview.cs
@@ -11,7 +11,20 @@ namespace Application.Services.Reviews.Dto
         public int Likes { get; set; }
         public string Title { get; set; }
         public string DescriptionReview { get; set; }
-        public IUser User { get; set; }
-        public IItem Item { get; set; }
+        public User Reviewer { get; set; }
+        public Item ItemReviewed { get; set; }
+
+        public class User
+        {
+            public int Id { get; set; }
+            public string Firstname { get; set; }
+            public string Lastname { get; set; }
+        }
+
+        public class Item
+        {
+            public int Id { get; set; }
+            public string Label { get; set; }
+        }
     }
 }

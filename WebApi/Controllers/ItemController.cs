8ce94a9bc504cfbec82a7546259629b195164ca0
la188043 <la188043@student.helha.be>
2020-11-28 12:14:30 +0100

    Added getbyid for items

---
+++ WebApi/Controllers/ItemController.cs
@@ -0,0 +1,25 @@
+﻿using Application.Services.Items;
+using Application.Services.Items.Dto;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApi.Controllers
+{
+    [ApiController]
+    [Route("api/items")]
+    public class ItemController : ControllerBase
+    {
+        private readonly IItemService _itemService;
+
+        public ItemController(IItemService itemService)
+        {
+            _itemService = itemService;
+        }
+
+        [HttpGet]
+        [Route("{id:int}")]
+        public ActionResult<OutputDtoQueryItem> Get(int id)
+        {
+            return Ok(_itemService.GetById(id));
+        }
+    }
+}

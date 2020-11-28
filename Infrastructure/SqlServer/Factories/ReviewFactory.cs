a134fd92f0c74d50a3de86e6ca3e5247db8e89d4
la188043 <la188043@student.helha.be>
2020-11-28 21:50:43 +0100

    Reviews: refactoring review sql reader factory to match dto

--- Infrastructure/SqlServer/Factories/ReviewFactory.cs
+++ Infrastructure/SqlServer/Factories/ReviewFactory.cs
@@ -20,28 +20,17 @@ namespace Infrastructure.SqlServer.Factories
                 Title = reader.GetString(reader.GetOrdinal(ReviewSqlServer.ColTitle)),
                 DescriptionReview = reader.GetString(reader.GetOrdinal(ReviewSqlServer.ColDescriptionReview)),
 
-                User = new User()
+                Reviewer = new User
                 {
-                    Id = reader.GetInt32(reader.GetOrdinal(ReviewSqlServer.ColId)),
+                    Id = reader.GetInt32(reader.GetOrdinal(ReviewSqlServer.ColUserId)),
                     Firstname = reader.GetString(reader.GetOrdinal(UserSqlServer.ColFirstname)),
-                    Lastname = reader.GetString(reader.GetOrdinal(UserSqlServer.ColLastname)),
-                    Birthdate = reader.GetDateTime(reader.GetOrdinal(UserSqlServer.ColBirthDate)),
-                    Email = reader.GetString(reader.GetOrdinal(UserSqlServer.ColEmail)),
-                    EncryptedPassword = reader.GetString(reader.GetOrdinal(UserSqlServer.ColPassword)),
-                    Gender = reader.GetString(reader.GetOrdinal(UserSqlServer.ColGender))[0],
-                    Administrator = reader.GetBoolean(reader.GetOrdinal(UserSqlServer.ColAdmin))
+                    Lastname = reader.GetString(reader.GetOrdinal(UserSqlServer.ColLastname))
                 },
-
-                Item = new Item()
+                
+                ItemReviewed = new Item
                 {
-                    Id = reader.GetInt32(reader.GetOrdinal(ReviewSqlServer.ColId)),
-                    Label = reader.GetString(reader.GetOrdinal(ItemSqlServer.ColLabel)),
-                    Price = (float) reader.GetDouble(reader.GetOrdinal(ItemSqlServer.ColPrice)),
-                    Quantity = reader.GetInt32(reader.GetOrdinal(ItemSqlServer.ColQuantity)),
-                    ImageItem = reader.GetString(reader.GetOrdinal(ItemSqlServer.ColImageItem)),
-                    DescriptionItem = reader.GetString(reader.GetOrdinal(ItemSqlServer.ColDescriptionItem)),
-                    Size = reader.GetString(reader.GetOrdinal(ItemSqlServer.ColSize)),
-                    // SubcategoryId = reader.GetInt32(reader.GetOrdinal(ItemSqlServer.ColSubCategoryId))
+                    Id = reader.GetInt32(reader.GetOrdinal(ReviewSqlServer.ColItemId)),
+                    Label = reader.GetString(reader.GetOrdinal(ItemSqlServer.ColLabel))
                 }
             };
         }

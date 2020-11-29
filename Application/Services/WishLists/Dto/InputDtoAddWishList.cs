d0a0c066e7882e4eea7a5fd21f6f909964df712b
youssraaa <youssradahouane1@gmail.com>
2020-11-29 19:40:22 +0100

    Services wishList verified AddedAt + values null

---
+++ Application/Services/WishLists/Dto/InputDtoAddWishList.cs
@@ -0,0 +1,11 @@
+﻿﻿using System;
+using Domain.Items;
+using Domain.Users;
+
+namespace Application.Services.WishLists.Dto
+{
+    public class InputDtoAddWishList
+    {
+        public DateTime AddedAt { get; }
+    }
+}

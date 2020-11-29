b2a5d9ea2256ae808358fd9a5ca934cb008ff170
la188043 <la188043@student.helha.be>
2020-11-29 14:46:27 +0100

    BaggedItem: input add dto and output query dto

---
+++ Application/Services/BaggedItems/Dto/InputDtoAddItemToBag.cs
@@ -0,0 +1,8 @@
+﻿namespace Application.Services.BaggedItems.Dto
+{
+    public class InputDtoAddItemToBag
+    {
+        public int Quantity { get; set; }
+        public string Size { get; set; }
+    }
+}

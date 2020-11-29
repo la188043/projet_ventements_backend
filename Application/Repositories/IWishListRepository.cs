2d74af3986c0e252485f137ed7ba9faca2ae08af
youssraaa <youssradahouane1@gmail.com>
2020-11-29 19:29:03 +0100

    WishListRepository : Query,GetByUserId,Add,Delete

---
+++ Application/Repositories/IWishListRepository.cs
@@ -0,0 +1,17 @@
+﻿﻿using System.Collections.Generic;
+using Domain.Addresses;
+using Domain.Items;
+using Domain.Users;
+using Domain.Wishlists;
+
+namespace Application.Repositories
+{
+    public interface IWishListRepository
+    {
+        IEnumerable<IWishlist> Query();
+        IEnumerable<IWishlist> GetByUserId(int uservId);
+        IWishlist Add(int uservId, int itemId, IWishlist wishList); 
+        bool Delete(int id);
+  
+    }
+}

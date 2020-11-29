9c9d368fd6168d20e059f6f759e237ea932aa85f
la188043 <la188043@student.helha.be>
2020-11-29 15:15:20 +0100

    BaggedItems: added class which is going to handle user bag in domain

---
+++ Domain/BaggedItems/UserBag.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Domain.Users;
+
+namespace Domain.BaggedItems
+{
+    public class UserBag : IUserBag
+    {
+        public IUser BagOwner { get; set; }
+        public IList<IBaggedItem> Items { get; }
+
+        public UserBag()
+        {
+            Items = new List<IBaggedItem>();
+        }
+
+        public void AddItem(IBaggedItem item)
+        {
+            Items.Add(item);
+        }
+
+        public void AddItems(IEnumerable<IBaggedItem> items)
+        {
+            foreach (var item in items)
+            {
+                Items.Add(item);
+            }
+        }
+
+        public float ComputeTotalPrice()
+        {
+            return Items.Select(item => item.AddedItem.Price).Sum();
+        }
+    }
+}

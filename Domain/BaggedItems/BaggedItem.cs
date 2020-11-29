82c1536aa4a5e3cca00a72aa41e528c2c3d1de3e
la188043 <la188043@student.helha.be>
2020-11-29 14:37:08 +0100

    BaggedItem: domain

---
+++ Domain/BaggedItems/BaggedItem.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Domain.Items;
+using Domain.Users;
+
+namespace Domain.BaggedItems
+{
+    public class BaggedItem : IBaggedItem
+    {
+        public int Id { get; set; }
+        public DateTime AddedAt { get; set; }
+        public int Quantity { get; set; }
+        public string Size { get; set; }
+        public IUser BagOwner { get; set; }
+        public IItem AddedItem { get; set; }
+
+        public BaggedItem()
+        {
+        }
+    }
+}

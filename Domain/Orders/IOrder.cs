66f54d87aeaa57990bdeeaf0e2d6611fb6a983c7
la188043 <la188043@student.helha.be>
2020-12-03 21:12:09 +0100

    Order Domain

---
+++ Domain/Orders/IOrder.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Domain.Shared;
+using Domain.Users;
+
+namespace Domain.Orders
+{
+    public interface IOrder : IEntity
+    {
+        bool IsPaid { get; set; }
+        DateTime orderedAt { get; set; }
+        IUser Orderer { get; set; }
+    }
+}

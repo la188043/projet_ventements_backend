28a4d55de97f5193f42d87a7152ad42a279fe710
la188043 <la188043@student.helha.be>
2020-12-04 20:30:04 +0100

    Order sql query get by user id

--- Infrastructure/SqlServer/Orders/OrderSqlServer.cs
+++ Infrastructure/SqlServer/Orders/OrderSqlServer.cs
@@ -1,4 +1,6 @@
-﻿namespace Infrastructure.SqlServer.Orders
+﻿using Infrastructure.SqlServer.Users;
+
+namespace Infrastructure.SqlServer.Orders
 {
     public class OrderSqlServer
     {
@@ -8,9 +10,17 @@
         public static readonly string ColOrderedAt = "orderedAt";
         public static readonly string ColUserId = "uservId";
 
-        public static readonly string ReqGetByUserId = $@"
-            SELECT 
+        public static readonly string ReqQuery = $@"
+            SELECT {TableName}.*,
+                   {UserSqlServer.TableName}.{UserSqlServer.ColFirstname},
+                   {UserSqlServer.TableName}.{UserSqlServer.ColLastname},
+                   {UserSqlServer.TableName}.{UserSqlServer.ColEmail},
+            FROM {TableName}
+            INNER JOIN {UserSqlServer.TableName}
+            ON {TableName}.{ColUserId} = {UserSqlServer.TableName}.{ColId}
         ";
+
+        public static readonly string ReqGetByUserId = ReqQuery + $" WHERE {TableName}.{ColUserId} = @{ColUserId}";
 
         public static readonly string ReqCreate = $@"
             INSERT INTO {TableName}

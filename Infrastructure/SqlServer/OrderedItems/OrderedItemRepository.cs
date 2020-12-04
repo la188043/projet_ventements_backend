32dbcab727c3981375962dd6536c928ba8d03712
la188043 <la188043@student.helha.be>
2020-12-04 20:36:40 +0100

    OrderedItem repo get by order id

--- Infrastructure/SqlServer/OrderedItems/OrderedItemRepository.cs
+++ Infrastructure/SqlServer/OrderedItems/OrderedItemRepository.cs
@@ -1,14 +1,34 @@
 ﻿using System.Collections.Generic;
+using System.Data;
 using Application.Repositories;
 using Domain.OrderedItems;
+using Infrastructure.SqlServer.Factories;
+using Infrastructure.SqlServer.Shared;
 
 namespace Infrastructure.SqlServer.OrderedItems
 {
     public class OrderedItemRepository : IOrderedItemRepository
     {
+        private IInstanceFromReader<IOrderedItem> _factory = new OrderedItemFactory();
+        
         public IEnumerable<IOrderedItem> GetByOrderId(int orderId)
         {
-            throw new System.NotImplementedException();
+            IList<IOrderedItem> orderedItems = new List<IOrderedItem>();
+            using (var connection = Database.GetConnection())
+            {
+                connection.Open();
+                var cmd = connection.CreateCommand();
+                cmd.CommandText = OrderedItemSqlServer.ReqGetByOrderId;
+
+                cmd.Parameters.AddWithValue($"@{OrderedItemSqlServer.ColOrderId}", orderId);
+
+                var reader = cmd.ExecuteReader(CommandBehavior.CloseConnection);
+                
+                while (reader.Read())
+                    orderedItems.Add(_factory.CreateFromReader(reader));
+            }
+
+            return orderedItems;
         }
     }
 }

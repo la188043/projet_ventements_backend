b2454b438b6a65b4db042052be49d595a6e983ce
la188043 <la188043@student.helha.be>
2020-12-04 20:07:09 +0100

    Ordered item sql query get by order id

--- Infrastructure/SqlServer/OrderedItems/OrderedItemSqlServer.cs
+++ Infrastructure/SqlServer/OrderedItems/OrderedItemSqlServer.cs
@@ -1,4 +1,7 @@
-﻿namespace Infrastructure.SqlServer.OrderedItems
+﻿using Infrastructure.SqlServer.Items;
+using Infrastructure.SqlServer.Orders;
+
+namespace Infrastructure.SqlServer.OrderedItems
 {
     public class OrderedItemSqlServer
     {
@@ -7,8 +10,23 @@
         public static readonly string ColOrderId = "ordervId";
         public static readonly string ColItemId = "itemId";
 
-        public static readonly string ReqQuery = $@"";
+        public static readonly string ReqQuery = $@"
+            SELECT {TableName}.*,
+                   {OrderSqlServer.TableName}.{OrderSqlServer.ColUserId},
+                   {OrderSqlServer.TableName}.{OrderSqlServer.ColIsPaid},
+                   {OrderSqlServer.TableName}.{OrderSqlServer.ColOrderedAt},
+                   {ItemSqlServer.TableName}.{ItemSqlServer.ColLabel},
+                   {ItemSqlServer.TableName}.{ItemSqlServer.ColPrice},
+                   {ItemSqlServer.TableName}.{ItemSqlServer.ColImageItem},
+                   {ItemSqlServer.TableName}.{ItemSqlServer.ColDescriptionItem},
+                   {ItemSqlServer.TableName}.{ItemSqlServer.ColCategoryId}
+            FROM {TableName}
+            INNER JOIN {OrderSqlServer.TableName}
+            ON {TableName}.{ColOrderId} = {OrderSqlServer.TableName}.{OrderSqlServer.ColId}
+            INNER JOIN {ItemSqlServer.TableName}
+            ON {TableName}.{ColItemId} = {ItemSqlServer.TableName}.{ItemSqlServer.ColId}
+        ";
         
-        public static readonly string ReqGetByOrderId = $@"";
+        public static readonly string ReqGetByOrderId = ReqQuery + $" WHERE {TableName}.{ColOrderId} = @{ColOrderId}";
     }
 }

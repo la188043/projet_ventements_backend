d702378f8fb266ed2f75cd705ce96894550ef3dd
la188043 <la188043@student.helha.be>
2020-12-05 01:01:36 +0100

    delete order in service

--- Application/Services/Orders/OrderService.cs
+++ Application/Services/Orders/OrderService.cs
@@ -100,7 +100,7 @@ namespace Application.Services.Orders
 
         public bool Delete(int orderId)
         {
-            throw new System.NotImplementedException();
+            return _orderRepository.Delete(orderId);
         }
     }
 }

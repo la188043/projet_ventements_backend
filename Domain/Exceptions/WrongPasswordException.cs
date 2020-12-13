92d88c57e266860b4f9527000efcd1190b0332ec
la188043 <la188043@student.helha.be>
2020-12-13 02:17:14 +0100

    Error handling: wrong password exception

---
+++ Domain/Exceptions/WrongPasswordException.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Domain.Exceptions
+{
+    public class WrongPasswordException : Exception
+    {
+        public WrongPasswordException(string? message) : base(message)
+        {
+            
+        }
+    }
+}

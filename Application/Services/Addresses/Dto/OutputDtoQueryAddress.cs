d144705bddee9588e6a1046113cd667cf3e4709f
la188043 <la188043@student.helha.be>
2020-12-15 14:24:31 +0100

    unit tests address service: generated equals in outputdto query address

--- Application/Services/Addresses/Dto/OutputDtoQueryAddress.cs
+++ Application/Services/Addresses/Dto/OutputDtoQueryAddress.cs
@@ -1,4 +1,6 @@
-﻿namespace Application.Services.Addresses.Dto
+﻿using System;
+
+namespace Application.Services.Addresses.Dto
 {
     public class OutputDtoQueryAddress
     {
@@ -7,21 +9,15 @@
         public int HomeNumber { get; set; }
         public string Zip { get; set; }
         public string City { get; set; }
-        
-        // TEST
+
+        private bool Equals(OutputDtoQueryAddress other)
+        {
+            return Id == other.Id && Street == other.Street && HomeNumber == other.HomeNumber && Zip == other.Zip && City == other.City;
+        }
+
         public override bool Equals(object obj)
         {
-            if (obj is OutputDtoQueryAddress address)
-            {
-                return
-                    Id == address.Id &&
-                    Street.Equals(address.Street) &&
-                    HomeNumber == address.HomeNumber &&
-                    Zip.Equals(address.Zip) &&
-                    City.Equals(address.City);
-            }
-
-            return false;
+            return ReferenceEquals(this, obj) || obj is OutputDtoQueryAddress other && Equals(other);
         }
     }
 }

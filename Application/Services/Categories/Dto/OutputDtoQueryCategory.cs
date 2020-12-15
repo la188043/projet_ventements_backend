7196b2fd312f5bd19d883d4c68f69721bceb714f
la188043 <la188043@student.helha.be>
2020-12-15 18:53:55 +0100

    unit tests category Service: Query + equals in dto

--- Application/Services/Categories/Dto/OutputDtoQueryCategory.cs
+++ Application/Services/Categories/Dto/OutputDtoQueryCategory.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 
 namespace Application.Services.Categories.Dto
 {
@@ -12,6 +13,40 @@ namespace Application.Services.Categories.Dto
         {
             public int Id { get; set; }
             public string Title { get; set; }
+
+            private bool Equals(Category other)
+            {
+                return Id == other.Id && Title == other.Title;
+            }
+
+            public override bool Equals(object obj)
+            {
+                if (ReferenceEquals(null, obj)) return false;
+                if (ReferenceEquals(this, obj)) return true;
+                if (obj.GetType() != this.GetType()) return false;
+                return Equals((Category) obj);
+            }
+        }
+
+        private bool Equals(OutputDtoQueryCategory other)
+        {
+            return Id == other.Id && Title == other.Title && ListEquals(SubCategories, other.SubCategories);
+        }
+
+        private bool ListEquals(IEnumerable<Category> subCategories, IEnumerable<Category> otherSubCategories)
+        {
+            var list = subCategories.ToList();
+            var other = otherSubCategories.ToList();
+
+            return list.Count == other.Count && list.All(cat => other.Contains(cat));
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(null, obj)) return false;
+            if (ReferenceEquals(this, obj)) return true;
+            if (obj.GetType() != this.GetType()) return false;
+            return Equals((OutputDtoQueryCategory) obj);
         }
     }
 }
---
+++ UnitTests/Application/CategoryServiceTest.cs
@@ -0,0 +1,101 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Application.Repositories;
+using Application.Services.Categories;
+using Application.Services.Categories.Dto;
+using Domain.Categories;
+using NSubstitute;
+using NUnit.Framework;
+
+namespace UnitTests.Application
+{
+    [TestFixture]
+    public class CategoryServiceTest
+    {
+        public static ICategory CreateParentCategory(int i)
+        {
+            return new Category
+            {
+                Id = i,
+                Title = i.ToString(),
+                ParentCategory = null
+            };
+        }
+
+        public static ICategory CreateSubcategory(ICategory parent, int i)
+        {
+            return new Category
+            {
+                Id = i,
+                Title = i.ToString(),
+                ParentCategory = parent
+            };
+        }
+
+        public static IEnumerable<ICategory> CreateListOfCategories(int offset, int sizeOfList)
+        {
+            IList<ICategory> categories = new List<ICategory>();
+            for (var i = offset; i < offset + sizeOfList; i++)
+            {
+                categories.Add(CreateParentCategory(i));
+            }
+
+            return categories;
+        }
+
+        public static IEnumerable<ICategory> CreateListOfSubs(ICategory parent, int sizeOfList)
+        {
+            IList<ICategory> categories = new List<ICategory>();
+            for (var i = 1; i <= sizeOfList; i++)
+            {
+                categories.Add(CreateSubcategory(parent, i));
+            }
+
+            return categories;
+        }
+        
+        // DTOs
+        public static InputDtoAddCategory CreateInputDtoAddCategory(int i)
+        {
+            return new InputDtoAddCategory {Title = i.ToString()};
+        }
+
+        public static OutputDtoAddCategory CreateOupOutputDtoAddCategory(int i)
+        {
+            return new OutputDtoAddCategory
+            {
+                Id = i,
+                Title = i.ToString()
+            };
+        }
+
+        public static OutputDtoQueryCategory CreateOutputDtoQueryCategory(int i)
+        {
+            return new OutputDtoQueryCategory
+            {
+                Id = i,
+                Title = i.ToString(),
+                SubCategories = new[] {new OutputDtoQueryCategory.Category {Id = 1, Title = "1"},new OutputDtoQueryCategory.Category {Id = 2, Title = "2"}}
+            };
+        }
+        
+        [Test]
+        public void Query_ReturnsParentCategoriesListWithSubcategoriesList()
+        {
+            // ARRANGE //
+            var categoryRep = Substitute.For<ICategoryRepository>();
+            categoryRep.Query().Returns(new[] {CreateParentCategory(1)});
+            categoryRep.GetByCategoryId(Arg.Any<int>()).Returns(CreateListOfSubs(CreateParentCategory(1), 2));
+            
+            var categoryService = new CategoryService(categoryRep);
+
+            var expected = new[] {CreateOutputDtoQueryCategory(1)};
+            
+            // ACT //
+            var output = categoryService.Query();
+            
+            // ASSERT //
+            Assert.AreEqual(expected, output);
+        }
+    }
+}

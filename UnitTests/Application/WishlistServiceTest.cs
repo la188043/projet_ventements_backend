48764f11cd56c89fa1f6bd7b7b4ae307aedb13f5
la188043 <la188043@student.helha.be>
2020-12-15 23:46:58 +0100

    unit tests wishlist Service: finished

--- UnitTests/Application/WishlistServiceTest.cs
+++ UnitTests/Application/WishlistServiceTest.cs
@@ -1,10 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
+using Application.Repositories;
+using Application.Services.WishLists;
 using Application.Services.WishLists.Dto;
 using Domain.Categories;
+using Domain.Exceptions;
 using Domain.Items;
 using Domain.Users;
 using Domain.Wishlists;
+using NSubstitute;
 using NUnit.Framework;
 
 namespace UnitTests.Application
@@ -84,6 +88,95 @@ namespace UnitTests.Application
             }
 
             return wishlists;
+        }
+
+        [Test]
+        public void GetById_SingleNumber_ReturnsSingleOutputDtoQueryWishlist()
+        {
+            // ARRANGE //
+            var wishlistRep = Substitute.For<IWishListRepository>();
+            wishlistRep.GetById(1).Returns(CreateWishlist(1));
+
+            var wishlistService = new WishListService(wishlistRep);
+            var expected = CreateOutputDtoQueryWishlist(1);
+
+            // ACT //
+            var output = wishlistService.GetById(1);
+
+            // ASSERT //
+            Assert.AreEqual(expected, output);
+        }
+
+        [Test]
+        [TestCase(0)]
+        [TestCase(3)]
+        [TestCase(7)]
+        [TestCase(23)]
+        public void GetByUserId_SingleNumber_ReturnsListOfOutputDtoQueryWishlist(int nbOfWishlistItems)
+        {
+            // ARRANGE //
+            var wishlistRep = Substitute.For<IWishListRepository>();
+            wishlistRep.GetByUserId(1).Returns(CreateListOfWishlist(nbOfWishlistItems));
+
+            var wishlistService = new WishListService(wishlistRep);
+            var expected = CreateListOfOutputDtoQueryWishlist(nbOfWishlistItems);
+
+            // ACT //
+            var output = wishlistService.GetByUserId(1);
+
+            // ASSERT //
+            Assert.AreEqual(expected, output);
+        }
+
+        [Test]
+        public void Add_UserIdAndItemId_ReturnsOutputDtoQueryWishlist()
+        {
+            // ARRANGE //
+            var wishlistRep = Substitute.For<IWishListRepository>();
+            wishlistRep.GetById(1).Returns(CreateWishlist(1));
+            wishlistRep.Add(1, 1).Returns(CreateWishlist(1));
+
+            var wishlistService = new WishListService(wishlistRep);
+            var expected = CreateOutputDtoQueryWishlist(1);
+
+            // ACT //
+            var output = wishlistService.Add(1, 1);
+
+            // ASSERT //
+            Assert.AreEqual(expected, output);
+        }
+
+        [Test]
+        public void Add_UserIdAndItemId_ThrowsExcpetion()
+        {
+            // ARRANGE //
+            var wishlistRep = Substitute.For<IWishListRepository>();
+            wishlistRep.GetById(1).Returns(CreateWishlist(1));
+            wishlistRep.Add(1, 1)
+                .Returns(x => { throw new DuplicateSqlPrimaryException("message"); });
+
+            var wishlistService = new WishListService(wishlistRep);
+            
+            // ASSERT //
+            Assert.Throws<DuplicateSqlPrimaryException>(() => wishlistService.Add(1, 1));
+        }
+
+        [Test]
+        [TestCase(true)]
+        [TestCase(false)]
+        public void Delete_SingleNumber_ReturnsIsDeleted(bool isDeletedFromRepo)
+        {
+            // ARRANGE //
+            var wishlistRep = Substitute.For<IWishListRepository>();
+            wishlistRep.Delete(1).Returns(isDeletedFromRepo);
+
+            var wishlistService = new WishListService(wishlistRep);
+            
+            // ACT //
+            var output = wishlistService.Delete(1);
+
+            // ASSERT //
+            Assert.AreEqual(isDeletedFromRepo, output);
         }
     }
 }

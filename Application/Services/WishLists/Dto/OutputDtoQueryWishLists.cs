7eef8bf7d0604d6ea58e9633c5d7e4f05501bb3f
la188043 <la188043@student.helha.be>
2020-12-15 23:18:24 +0100

    unit tests wishlist service: created test class + equals in output dto

--- Application/Services/WishLists/Dto/OutputDtoQueryWishLists.cs
+++ Application/Services/WishLists/Dto/OutputDtoQueryWishLists.cs
@@ -18,6 +18,19 @@ namespace Application.Services.WishLists.Dto
             public int Id { get; set; }
             public string Firstname { get; set; }
             public string Lastname { get; set; }
+
+            private bool Equals(User other)
+            {
+                return Id == other.Id && Firstname == other.Firstname && Lastname == other.Lastname;
+            }
+
+            public override bool Equals(object obj)
+            {
+                if (ReferenceEquals(null, obj)) return false;
+                if (ReferenceEquals(this, obj)) return true;
+                if (obj.GetType() != this.GetType()) return false;
+                return Equals((User) obj);
+            }
         }
 
         public class Item
@@ -27,6 +40,34 @@ namespace Application.Services.WishLists.Dto
             public float Price { get; set; }
             public string ImageItem { get; set; }
             public string DescriptionItem { get; set; }
+
+            private bool Equals(Item other)
+            {
+                return Id == other.Id && Label == other.Label && Price.Equals(other.Price) &&
+                       ImageItem == other.ImageItem && DescriptionItem == other.DescriptionItem;
+            }
+
+            public override bool Equals(object obj)
+            {
+                if (ReferenceEquals(null, obj)) return false;
+                if (ReferenceEquals(this, obj)) return true;
+                if (obj.GetType() != this.GetType()) return false;
+                return Equals((Item) obj);
+            }
+        }
+
+        private bool Equals(OutputDtoQueryWishLists other)
+        {
+            return Id == other.Id && UserWishList.Equals(other.UserWishList) &&
+                   ItemWishList.Equals(other.ItemWishList);
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(null, obj)) return false;
+            if (ReferenceEquals(this, obj)) return true;
+            if (obj.GetType() != this.GetType()) return false;
+            return Equals((OutputDtoQueryWishLists) obj);
         }
     }
 }

412b36d8790844b40a52f782930902040cb232db
la188043 <la188043@student.helha.be>
2020-12-15 16:54:47 +0100

    unit tests baggedItem Service: GetByUserId

--- UnitTests/Application/BaggedItemServiceTest.cs
+++ UnitTests/Application/BaggedItemServiceTest.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using Application.Repositories;
+using Application.Services.BaggedItems;
+using Application.Services.BaggedItems.Dto;
 using Domain.Addresses;
 using Domain.BaggedItems;
 using Domain.Categories;
@@ -14,7 +17,7 @@ namespace UnitTests.Application
     [TestFixture]
     public class BaggedItemServiceTest
     {
-        // Helper static methods
+        // Helper static methods (Domain instances)
         public static IUser CreateUser(int i)
         {
             return new User
@@ -55,12 +58,12 @@ namespace UnitTests.Application
         {
             return new BaggedItem
             {
-                 Id = i,
-                 Quantity = i,
-                 Size = i.ToString(),
-                 AddedAt = DateTime.Now,               
-                 BagOwner = CreateUser(i),
-                 AddedItem = CreateItem(i)
+                Id = i,
+                Quantity = i,
+                Size = i.ToString(),
+                AddedAt = DateTime.Now,
+                BagOwner = CreateUser(1),
+                AddedItem = CreateItem(i)
             };
         }
 
@@ -75,6 +78,43 @@ namespace UnitTests.Application
             return baggedItems;
         }
 
+        // Helper static methods (DTO instances)
+        public static OutputDtoQueryUserBaggedItem CreateOutputDtoQueryUserBaggedItem(int i, int listSize)
+        {
+            var userBag = new UserBag();
+            userBag.AddItems(CreateListOfBaggedItems(listSize));
+            var dtoBaggedItems = userBag.Items
+                .Select(baggedItem => new OutputDtoQueryUserBaggedItem.BaggedItem
+                {
+                    Id = baggedItem.Id,
+                    AddedAt = baggedItem.AddedAt,
+                    Quantity = baggedItem.Quantity,
+                    Size = baggedItem.Size,
+
+                    BagItem = new OutputDtoQueryUserBaggedItem.BaggedItem.Item
+                    {
+                        Id = baggedItem.AddedItem.Id,
+                        Label = baggedItem.AddedItem.Label,
+                        Price = baggedItem.AddedItem.Price * baggedItem.Quantity,
+                        ImageItem = baggedItem.AddedItem.ImageItem,
+                        DescriptionItem = baggedItem.AddedItem.DescriptionItem
+                    }
+                });
+
+            var bagOwner = CreateUser(1);
+             return new OutputDtoQueryUserBaggedItem
+             {
+                 BagOwner = new OutputDtoQueryUserBaggedItem.User
+                 {
+                     Id = bagOwner.Id,
+                     Firstname = bagOwner.Firstname,
+                     Lastname = bagOwner.Lastname
+                 },
+                 TotalPrice = userBag.ComputeTotalPrice(),
+                 Items = dtoBaggedItems
+             };           
+        }
+
         [Test]
         public void GetByUserId_SingleNumber_ReturnsOutputDtoQueryUserBaggedItem()
         {
@@ -86,11 +126,20 @@ namespace UnitTests.Application
             var baggedItemRep = Substitute.For<IBaggedItemRepository>();
 
             // Substitutes behavior
-            
+            baggedItemRep.GetByUserId(1).Returns(CreateListOfBaggedItems(2));
+            userRep.GetById(1).Returns(CreateUser(1));
+
+            // BaggedItem service
+            var baggedItemService = new BaggedItemService(baggedItemRep, userRep, itemRep);
+
+            // Expectation
+            var expected = CreateOutputDtoQueryUserBaggedItem(1, 2);
 
             // ACT //
+            var outputBaggedItems = baggedItemService.GetByUserId(1);
 
             // ASSERT //
+            Assert.AreEqual(expected, outputBaggedItems);
         }
     }
 }

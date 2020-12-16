66ff8249667f8ddead8bdaa11544c39811320264
la188043 <la188043@student.helha.be>
2020-12-16 16:47:01 +0100

    unit tests orderedItem services: equals in output dto + helper methods

--- Application/Services/OrderedItems/Dto/OutputDtoQueryOrderedItem.cs
+++ Application/Services/OrderedItems/Dto/OutputDtoQueryOrderedItem.cs
@@ -20,6 +20,34 @@
             public string DescriptionItem { get; set; }
             public int Quantity { get; set; }
             public string Size { get; set; }
+
+            private bool Equals(Item other)
+            {
+                return Id == other.Id && Label == other.Label && Price.Equals(other.Price) &&
+                       ImageItem == other.ImageItem && DescriptionItem == other.DescriptionItem &&
+                       Quantity == other.Quantity && Size == other.Size;
+            }
+
+            public override bool Equals(object obj)
+            {
+                if (ReferenceEquals(null, obj)) return false;
+                if (ReferenceEquals(this, obj)) return true;
+                if (obj.GetType() != this.GetType()) return false;
+                return Equals((Item) obj);
+            }
+        }
+
+        private bool Equals(OutputDtoQueryOrderedItem other)
+        {
+            return Id == other.Id && ItemOrder.Equals(other.ItemOrder) && ItemOrdered.Equals(other.ItemOrdered);
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(null, obj)) return false;
+            if (ReferenceEquals(this, obj)) return true;
+            if (obj.GetType() != this.GetType()) return false;
+            return Equals((OutputDtoQueryOrderedItem) obj);
         }
     }
 }
--- UnitTests/Application/OrderedItemServiceTest.cs
+++ UnitTests/Application/OrderedItemServiceTest.cs
@@ -1,10 +1,102 @@
-﻿using NUnit.Framework;
+﻿using System.Collections.Generic;
+using Application.Services.OrderedItems.Dto;
+using Domain.Items;
+using Domain.OrderedItems;
+using Domain.Orders;
+using NUnit.Framework;
 
 namespace UnitTests.Application
 {
     [TestFixture]
     public class OrderedItemServiceTest
     {
-        
+        public static IOrderedItem CreateOrderedItem(int i)
+        {
+            return new OrderedItem
+            {
+                Id = i,
+                Quantity = i,
+                Size = i.ToString(),
+                ItemOrdered = new Item
+                {
+                    Id = i,
+                    Label = $"Item{i}",
+                    Price = i,
+                    ImageItem = i.ToString(),
+                    DescriptionItem = i.ToString(),
+                },
+                Order = new Order
+                {
+                    Id = i
+                }
+            };
+        }
+
+        public static IEnumerable<IOrderedItem> CreateListOfOrderedItems(int sizeOfList)
+        {
+            IList<IOrderedItem> items = new List<IOrderedItem>();
+            for (var i = 1; i <= sizeOfList; i++)
+            {
+                items.Add(CreateOrderedItem(i));
+            }
+
+            return items;
+        }
+
+        public static OutputDtoQueryOrderedItem CreateOutputDtoQueryOrderedItem(int i)
+        {
+            return new OutputDtoQueryOrderedItem
+            {
+                Id = i,
+                ItemOrder = new OutputDtoQueryOrderedItem.Order {Id = i},
+                ItemOrdered = new OutputDtoQueryOrderedItem.Item
+                {
+                    Id = i,
+                    Label = $"Item{i}",
+                    Price = i,
+                    ImageItem = i.ToString(),
+                    DescriptionItem = i.ToString(),
+                    Quantity = i,
+                    Size = i.ToString()
+                }
+            };
+        }
+
+        public static IEnumerable<OutputDtoQueryOrderedItem> CreateListOfOutputDtoQueryOrderedItems(int sizeOfList)
+        {
+            IList<OutputDtoQueryOrderedItem> items = new List<OutputDtoQueryOrderedItem>();
+            for (var i = 1; i <= sizeOfList; i++)
+            {
+                items.Add(CreateOutputDtoQueryOrderedItem(i));
+            }
+
+            return items;
+        }
+
+        public static InputDtoAddOrderedItem CreateInputDtoAddOrderedItem(int i)
+        {
+            return new InputDtoAddOrderedItem { Quantity = i, Size = i.ToString() };
+        }
+
+        public static InputDtoAddOrderedItems CreateInputDtoAddOrderedItems(int nbOfOrderedItems)
+        {
+            IList<InputDtoAddOrderedItems.OrderedItem> items = new List<InputDtoAddOrderedItems.OrderedItem>();
+            for (var i = 1; i <= nbOfOrderedItems; i++)
+            {
+                items.Add(new InputDtoAddOrderedItems.OrderedItem
+                {
+                    ItemId = i,
+                    Quantity = i,
+                    Size = i.ToString()
+                });
+            }
+
+            return new InputDtoAddOrderedItems {OrderedItems = items};
+        }
+
+        public static InputDtoUpdateOrderedItem CreateInputDtoUpdateOrderedItem(int i)
+        {
+            return new InputDtoUpdateOrderedItem {Quantity = i};
+        }
     }
 }

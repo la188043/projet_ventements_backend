a0b87cc2a3de9356e90692b9576fadef93f5294c
la188043 <la188043@student.helha.be>
2020-12-16 20:20:07 +0100

    unit tests order services: equals in ouput dtos

--- Application/Services/Orders/Dto/OutputDtoAddOrder.cs
+++ Application/Services/Orders/Dto/OutputDtoAddOrder.cs
@@ -7,5 +7,18 @@ namespace Application.Services.Orders.Dto
         public int Id { get; set; }
         public bool isPaid { get; set; }
         public DateTime orderedAt { get; set; }
+
+        private bool Equals(OutputDtoAddOrder other)
+        {
+            return Id == other.Id && isPaid == other.isPaid;
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(null, obj)) return false;
+            if (ReferenceEquals(this, obj)) return true;
+            if (obj.GetType() != this.GetType()) return false;
+            return Equals((OutputDtoAddOrder) obj);
+        }
     }
 }
--- Application/Services/Orders/Dto/OutputDtoQueryOrder.cs
+++ Application/Services/Orders/Dto/OutputDtoQueryOrder.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace Application.Services.Orders.Dto
 {
@@ -18,6 +19,20 @@ namespace Application.Services.Orders.Dto
             public string Firstname { get; set; }
             public string Lastname { get; set; }
             public string Email { get; set; }
+
+            private bool Equals(User other)
+            {
+                return Id == other.Id && Firstname == other.Firstname && Lastname == other.Lastname &&
+                       Email == other.Email;
+            }
+
+            public override bool Equals(object obj)
+            {
+                if (ReferenceEquals(null, obj)) return false;
+                if (ReferenceEquals(this, obj)) return true;
+                if (obj.GetType() != this.GetType()) return false;
+                return Equals((User) obj);
+            }
         }
 
         public class Item
@@ -30,6 +45,43 @@ namespace Application.Services.Orders.Dto
             public string DescriptionItem { get; set; }
             public int Quantity { get; set; }
             public string Size { get; set; }
+
+            private bool Equals(Item other)
+            {
+                return Id == other.Id && ItemId == other.ItemId && Label == other.Label && Price.Equals(other.Price) &&
+                       ImageItem == other.ImageItem && DescriptionItem == other.DescriptionItem &&
+                       Quantity == other.Quantity && Size == other.Size;
+            }
+
+            public override bool Equals(object obj)
+            {
+                if (ReferenceEquals(null, obj)) return false;
+                if (ReferenceEquals(this, obj)) return true;
+                if (obj.GetType() != this.GetType()) return false;
+                return Equals((Item) obj);
+            }
+        }
+
+        private bool Equals(OutputDtoQueryOrder other)
+        {
+            return Id == other.Id && IsPaid == other.IsPaid && TotalPrice.Equals(other.TotalPrice) &&
+                   Ordered.Equals(other.Ordered) && ListEquals(OrderedItems, other.OrderedItems);
+        }
+
+        private bool ListEquals(IEnumerable<Item> orderedItems, IEnumerable<Item> otherOrderedItems)
+        {
+            var list = orderedItems.ToList();
+            var other = otherOrderedItems.ToList();
+
+            return list.Count == other.Count && list.All(item => other.Contains(item));
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(null, obj)) return false;
+            if (ReferenceEquals(this, obj)) return true;
+            if (obj.GetType() != this.GetType()) return false;
+            return Equals((OutputDtoQueryOrder) obj);
         }
     }
 }

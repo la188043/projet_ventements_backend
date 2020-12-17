a6643a9131eff34a3958677efaf24deb2cc7ad78
la188043 <la188043@student.helha.be>
2020-12-17 21:21:52 +0100

    review query: query in repo

--- Application/Repositories/IReviewRepository.cs
+++ Application/Repositories/IReviewRepository.cs
@@ -7,6 +7,7 @@ namespace Application.Repositories
 {
     public interface IReviewRepository
     {
+        IEnumerable<IReview> Query();
         IReview GetById(int id);
         IEnumerable<IReview> GetByItemId(int itemId);
         IReview Create(int uservId, int itemId, IReview review);
